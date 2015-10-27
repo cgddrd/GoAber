cab0995531cb1e642ce98c9965c4ef96d4968884
Helen Harman <Helen Harman>
2015-10-27 17:54:30 +0000

    moved controllers to controller folder

--- src/ASP.NET/GoAber/GoAber/ModelGoAber.Designer.cs
+++ src/ASP.NET/GoAber/GoAber/ModelGoAber.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\helen\Documents\HelenVSprojects\GoAber\GoAber\ModelGoAber.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\helen\Documents\GoAber\src\ASP.NET\GoAber\GoAber\ModelGoAber.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
