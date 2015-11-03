83cc9063c2cdf61307f8f7bf7cb89123aa8e3b92
Samuel Jackson <samueljackson@outlook.com>
2015-11-03 22:56:07 +0000

    Refs #91 Change some display names.

--- src/ASP.NET/GoAber/GoAber/unit.cs
+++ src/ASP.NET/GoAber/GoAber/unit.cs
@@ -11,7 +11,8 @@ namespace GoAber
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+
     public partial class Unit
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,7 @@ namespace GoAber
         }
     
         public int idUnit { get; set; }
+        [DisplayName("Name")]
         public string name { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
