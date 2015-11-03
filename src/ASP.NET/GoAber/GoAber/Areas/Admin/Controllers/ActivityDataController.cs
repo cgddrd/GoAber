61f75d4981be6ed8289252d6972c46b9fdfcbe9b
Samuel Jackson <samueljackson@outlook.com>
2015-11-03 22:50:52 +0000

    Refs #91 Add admin area and scaffold activity data.

---
+++ src/ASP.NET/GoAber/GoAber/Areas/Admin/Controllers/ActivityDataController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GoAber;
+
+namespace GoAber.Areas.Admin.Controllers
+{
+    public class ActivityDataController : Controller
+    {
+        private GoAberEntities db = new GoAberEntities();
+
+        // GET: Admin/ActivityData
+        public ActionResult Index()
+        {
+            var activityDatas1 = db.ActivityDatas.Include(a => a.categoryunit).Include(a => a.user);
+            return View(activityDatas1.ToList());
+        }
+
+        // GET: Admin/ActivityData/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityData activityData = db.ActivityDatas.Find(id);
+            if (activityData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityData);
+        }
+
+        // GET: Admin/ActivityData/Create
+        public ActionResult Create()
+        {
+            ViewBag.categoryUnitId = new SelectList(db.CategoryUnits, "idCategoryUnit", "idCategoryUnit");
+            ViewBag.userId = new SelectList(db.Users, "idUser", "email");
+            return View();
+        }
+
+        // POST: Admin/ActivityData/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idActivityData,categoryUnitId,userId,value,lastUpdated,date")] ActivityData activityData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ActivityDatas.Add(activityData);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.categoryUnitId = new SelectList(db.CategoryUnits, "idCategoryUnit", "idCategoryUnit", activityData.categoryUnitId);
+            ViewBag.userId = new SelectList(db.Users, "idUser", "email", activityData.userId);
+            return View(activityData);
+        }
+
+        // GET: Admin/ActivityData/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityData activityData = db.ActivityDatas.Find(id);
+            if (activityData == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.categoryUnitId = new SelectList(db.CategoryUnits, "idCategoryUnit", "idCategoryUnit", activityData.categoryUnitId);
+            ViewBag.userId = new SelectList(db.Users, "idUser", "email", activityData.userId);
+            return View(activityData);
+        }
+
+        // POST: Admin/ActivityData/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idActivityData,categoryUnitId,userId,value,lastUpdated,date")] ActivityData activityData)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(activityData).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.categoryUnitId = new SelectList(db.CategoryUnits, "idCategoryUnit", "idCategoryUnit", activityData.categoryUnitId);
+            ViewBag.userId = new SelectList(db.Users, "idUser", "email", activityData.userId);
+            return View(activityData);
+        }
+
+        // GET: Admin/ActivityData/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ActivityData activityData = db.ActivityDatas.Find(id);
+            if (activityData == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activityData);
+        }
+
+        // POST: Admin/ActivityData/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ActivityData activityData = db.ActivityDatas.Find(id);
+            db.ActivityDatas.Remove(activityData);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
