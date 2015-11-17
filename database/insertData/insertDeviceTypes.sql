227ad818e111a96f26bd2adf88221be3b4e8758c
Helen Harman <Helen Harman>
2015-11-17 16:14:44 +0000

    insert devices into db

--- database/insertData/insertDeviceTypes.sql
+++ database/insertData/insertDeviceTypes.sql
@@ -1,3 +1,8 @@
-INSERT INTO UserRoles (type) VALUES ('administrator');
-INSERT INTO Users (email, nickname, userRoleId) values ('myEmail', 'test', '1');
-INSERT INTO DeviceTypes (name, tokenEndpoint, consumerKey, consumerSecret, clientId, authorizationEndpoint) values ('fitbit', 'https://api.fitbit.com/oauth2/token', 'e06d4e7dcbc6fc80c0d00b187b6fb2e1', 'bafe21eca0c10cfe54f21e9b685f041f', '229R69', 'https://www.fitbit.com/oauth2/authorize');
+/*
+-- Query: SELECT * FROM goaber.devicetype
+LIMIT 0, 1000
+
+-- Date: 2015-11-17 16:14
+*/
+INSERT INTO `devicetype` (`idDeviceType`,`name`,`tokenEndpoint`,`consumerKey`,`consumerSecret`,`clientId`,`authorizationEndpoint`) VALUES (1,'Jawbone','https://jawbone.com/auth/oauth2/token','','07e4083f111f1a44ccba1bf94d21c95f5486f8f1','mCZQ7V2DbgQ','https://jawbone.com/auth/oauth2/auth');
+INSERT INTO `devicetype` (`idDeviceType`,`name`,`tokenEndpoint`,`consumerKey`,`consumerSecret`,`clientId`,`authorizationEndpoint`) VALUES (NULL,'Fitbit','https://api.fitbit.com/oauth2/token',NULL,'6f961c38f86be0a77f903ca9a2524865','22B2TQ','https://www.fitbit.com/oauth2/authorize ');
