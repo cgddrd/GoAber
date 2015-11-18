273ffe4ea9023efbeb8dd0fd571e831f2abe24d4
Connor Goddard <hello@connorlukegoddard.com>
2015-11-18 07:47:07 +0000

    Added GF configuration file and tidied up SQL scripts.

--- database/insertData/insertUsers.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `goaber`.`user` (`email`, `nickname`, `userRoleId`) VALUES ('heh14@aber.ac.uk', 'heh14', '3');
-INSERT INTO `goaber`.`user` (`email`, `nickname`, `userRoleId`) VALUES ('myEmail', 'test', '1');
