404bf8228a92ab205ffb9c7ecc37ee9b04ef93ae
Samuel Jackson <samueljackson@outlook.com>
2015-11-19 10:04:56 +0000

    Refs #134 Add date range filtering

---
+++ src/JavaEE/GoAber/GoAber-war/src/java/JSF/util/DateRangeFilter.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JSF.util;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="dateRangeFilter")
+@ApplicationScoped
+public class DateRangeFilter implements Serializable {
+
+    private static final Logger LOG = Logger.getLogger(DateRangeFilter.class.getName());
+
+    public boolean filterByDate(Object value, Object filter, Locale locale) {
+
+        String filterText = (filter == null) ? null : filter.toString().trim();
+        if (filterText == null || filterText.isEmpty()) {
+            return true;
+        }
+        if (value == null) {
+            return false;
+        }
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yyy");
+
+        Date filterDate = (Date) value;
+        Date dateFrom;
+        Date dateTo;
+        try {
+            String fromPart = filterText.substring(0, filterText.indexOf("-"));
+            String toPart = filterText.substring(filterText.indexOf("-") + 1);
+            dateFrom = fromPart.isEmpty() ? null : df.parse(fromPart);
+            dateTo = toPart.isEmpty() ? null : df.parse(toPart);
+        } catch (ParseException pe) {
+            return false;
+        }
+
+        return (dateFrom == null || filterDate.after(dateFrom)) && (dateTo == null || filterDate.before(dateTo));
+    }
+}
