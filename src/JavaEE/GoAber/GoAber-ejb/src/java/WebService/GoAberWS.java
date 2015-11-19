c87f7c561e51837d13cf1ca4d24f3c310d847759
Dan Mcguckin <Dan Mcguckin>
2015-11-19 19:36:09 +0000

    Initial commit of branch.

---
+++ src/JavaEE/GoAber/GoAber-ejb/src/java/WebService/GoAberWS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WebService;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Dan
+ */
+@WebService(serviceName = "GoAberWS")
+@Stateless()
+public class GoAberWS {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
