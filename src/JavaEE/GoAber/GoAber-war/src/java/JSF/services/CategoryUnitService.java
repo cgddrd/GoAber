78ff181d40b051f2c5813bc7c2ed51e71a799f6a
Samuel Jackson <samueljackson@outlook.com>
2015-11-21 15:37:14 +0000

    Refs #159 Fix move functions to category unit service

---
+++ src/JavaEE/GoAber/GoAber-war/src/java/JSF/services/CategoryUnitService.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JSF.services;
+
+import GoAberDatabase.Category;
+import GoAberDatabase.CategoryUnit;
+import GoAberDatabase.Unit;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
+
+/**
+ *
+ * @author samuel
+ */
+@Stateless
+@LocalBean
+@ManagedBean(name="categoryUnitService")
+public class CategoryUnitService {
+
+    @EJB
+    SessionBean.CategoryUnitFacade ejbFacade;
+    @EJB
+    SessionBean.CategoryFacade categoryFacade;
+    @EJB
+    SessionBean.UnitFacade unitFacade;
+    
+    public List<SelectItem> getDisplayItems() {
+        List<SelectItem> itemsList = new ArrayList<>();
+        List<Category> categories = categoryFacade.findAll();
+        for(Category category : categories) {
+            SelectItemGroup group = new SelectItemGroup(category.getName());
+            SelectItem[] selectItems = createUnitsForCategory(category.getIdCategory());
+            group.setSelectItems(selectItems);
+            itemsList.add(group);
+        }
+        return itemsList;
+    }
+    
+    private SelectItem[] createUnitsForCategory(int id) {
+        List<CategoryUnit> categoryUnits = ejbFacade.findByCategory(id);
+        SelectItem[] selectItems = new SelectItem[categoryUnits.size()];
+        int i =0;
+        for(CategoryUnit categoryUnitItem : categoryUnits) {
+            Unit unit = unitFacade.find(categoryUnitItem.getUnitId().getIdUnit());
+            SelectItem item = new SelectItem(categoryUnitItem, unit.getName());
+            selectItems[i] = item;
+            i++;
+        }
+        return selectItems;
+    }
+}
