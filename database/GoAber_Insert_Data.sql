c1167cdc12de00e466d9ce33844bf8fdaa6c2538
Connor Goddard <hello@connorlukegoddard.com>
2015-11-27 11:07:41 +0000

    Updated MySQL seed files to remove duplicate reference to TeamId, and updated seed data to add test communities, community teams and category units.

--- database/GoAber_Insert_Data.sql
+++ database/GoAber_Insert_Data.sql
@@ -16,9 +16,36 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 USE `goaber`;
---
--- Dumping data for table `Role`
---
+
+LOCK TABLES `Community` WRITE;
+/*!40000 ALTER TABLE `Community` DISABLE KEYS */;
+INSERT INTO `Community` VALUES (1,'AberUni', 'http://aber.ac.uk'), (2,'BangorUni', 'http://bangor.ac.uk');
+/*!40000 ALTER TABLE `Community` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `Team` WRITE;
+/*!40000 ALTER TABLE `Team` DISABLE KEYS */;
+INSERT INTO `Team` VALUES (1,'AberCompSci', 1), (2,'AberIBERS', 1), (3,'BangorCompSci', 2), (4,'BangorBiology', 2);
+/*!40000 ALTER TABLE `Team` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `Category` WRITE;
+/*!40000 ALTER TABLE `Category` DISABLE KEYS */;
+INSERT INTO `Category` VALUES (1,'Running'), (2,'Swimming');
+/*!40000 ALTER TABLE `Category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `Unit` WRITE;
+/*!40000 ALTER TABLE `Unit` DISABLE KEYS */;
+INSERT INTO `Unit` VALUES (1,'Steps'), (2,'Strokes');
+/*!40000 ALTER TABLE `Unit` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `CategoryUnit` WRITE;
+/*!40000 ALTER TABLE `CategoryUnit` DISABLE KEYS */;
+INSERT INTO `CategoryUnit` VALUES (1,1,1), (2,2,2);
+/*!40000 ALTER TABLE `CategoryUnit` ENABLE KEYS */;
+UNLOCK TABLES;
 
 LOCK TABLES `Role` WRITE;
 /*!40000 ALTER TABLE `Role` DISABLE KEYS */;
@@ -26,19 +53,11 @@ INSERT INTO `Role` VALUES ('administrator'),('coordinator'),('participant');
 /*!40000 ALTER TABLE `Role` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping data for table `User`
---
-
 LOCK TABLES `User` WRITE;
 /*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES (1,'admin@test.com','J8xplPwcAc5mWca93Km2nExqlBgGXmEsadEQs/exH4o=','admin',1,NULL,NULL,'administrator'),(2,'coord@test.com','J8xplPwcAc5mWca93Km2nExqlBgGXmEsadEQs/exH4o=','coord',2,NULL,NULL,'admin'),(3,'user@test.com','J8xplPwcAc5mWca93Km2nExqlBgGXmEsadEQs/exH4o=','user',3,NULL,NULL,'participant');
+INSERT INTO `User` VALUES (1,'admin@test.com','+QUCS9lsdwiBAJ/YeyLdjWgxWVByMSolHNP6N/aGG8U=','admin',1,1),(2,'coord@test.com','+QUCS9lsdwiBAJ/YeyLdjWgxWVByMSolHNP6N/aGG8U=','coord',2,4),(3,'user@test.com','+QUCS9lsdwiBAJ/YeyLdjWgxWVByMSolHNP6N/aGG8U=','user',3,2);
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping data for table `UserRole`
---
 
 LOCK TABLES `UserRole` WRITE;
 /*!40000 ALTER TABLE `UserRole` DISABLE KEYS */;
@@ -47,9 +66,6 @@ INSERT INTO `UserRole` VALUES (1,'administrator','admin@test.com'),(2,'coordinat
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Dumping data for table 'DeviceType'
---
 INSERT INTO `devicetype` (`name`,`tokenEndpoint`,`consumerKey`,`consumerSecret`,`clientId`,`authorizationEndpoint`, `apiEndpoint`) VALUES ('Jawbone','https://jawbone.com/auth/oauth2/token','','07e4083f111f1a44ccba1bf94d21c95f5486f8f1','mCZQ7V2DbgQ','https://jawbone.com/auth/oauth2/auth', 'https://jawbone.com/nudge/api/v.1.1/users/@me');
 INSERT INTO `devicetype` (`name`,`tokenEndpoint`,`consumerKey`,`consumerSecret`,`clientId`,`authorizationEndpoint`, `apiEndpoint`) VALUES ('Fitbit','https://api.fitbit.com/oauth2/token',NULL,'6f961c38f86be0a77f903ca9a2524865','22B2TQ','https://www.fitbit.com/oauth2/authorize ', 'https://jawbone.com/nudge/api/v.1.1/users/@me');
 
