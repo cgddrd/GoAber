f292204e6bfd4b4d8b1c3b1b58efb92145c87986
Samuel Jackson <samueljackson@outlook.com>
2015-12-08 10:24:53 +0000

    Add leaderboards back again.

--- src/ASP.NET/GoAber/Resources/Resources.Designer.cs
+++ src/ASP.NET/GoAber/Resources/Resources.Designer.cs
@@ -1331,14 +1331,6 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Back to device management.
-        /// </summary>
-        public static string Device_DeleteBack {
-            get {
-                return ResourceManager.GetString("Device_DeleteBack", resourceCulture);
-			}
-		}
-		
         ///   Looks up a localized string similar to Create Connection.
         /// </summary>
         public static string Device_ConnectToDevice {
@@ -1348,13 +1340,15 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to has had its connection revoked..
+        ///   Looks up a localized string similar to Back to device management.
         /// </summary>
-        public static string Device_DeleteConnectionRevoked {
+        public static string Device_DeleteBack {
             get {
-                return ResourceManager.GetString("Device_DeleteConnectionRevoked", resourceCulture);
-			}
-		}
+                return ResourceManager.GetString("Device_DeleteBack", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Revoke Access.
         /// </summary>
         public static string Device_DeleteConnection {
@@ -1364,13 +1358,24 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to has had its connection revoked..
+        /// </summary>
+        public static string Device_DeleteConnectionRevoked {
+            get {
+                return ResourceManager.GetString("Device_DeleteConnectionRevoked", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Device connections.
         /// </summary>
         public static string Device_DeleteTitle {
             get {
                 return ResourceManager.GetString("Device_DeleteTitle", resourceCulture);
-			}
-		}
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Connect to FitBit.
         /// </summary>
         public static string Device_FitBitConnect {
